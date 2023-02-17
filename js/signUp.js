6de51de00b14bd2b8f9cd00536038d5d86fd1412
David Lidebrandt <david.lidebrandt@gmail.com>
2023-02-17 22:54:55 +0100

    Add password validation

--- js/signUp.js
+++ js/signUp.js
@@ -2,6 +2,7 @@ const signUpButton = document.querySelector(".signUpButton");
 const userName = document.querySelector("#user_name");
 const password = document.querySelector("#password");
 const email = document.querySelector("#email");
+const passwordIndicator = document.querySelector(".password_indicator");
 
 function submitSignUp() {
   alert(
@@ -15,4 +16,21 @@ function submitSignUp() {
 signUpButton.addEventListener("click", (event) => {
   event.preventDefault();
   submitSignUp();
+});
+
+password.addEventListener("change", (event)=> {
+    const containsNumbers = /(?=.*[0-9])/.test(password.value);
+    const containsSpecialCharacters = /(?=.*[!@#$%^&*])/.test(password.value);
+    const longEnoughPassword = password.value.length > 7;
+    
+    if(longEnoughPassword && containsNumbers && containsSpecialCharacters) {
+        passwordIndicator.textContent = "Strong password";
+        passwordIndicator.classList.add("green-text");
+        passwordIndicator.classList.remove("red-text");
+        return;
+    }
+    
+    passwordIndicator.classList.remove("green-text");
+    passwordIndicator.classList.add("red-text");
+    passwordIndicator.textContent = "Weak password, must be at least 8 characters, contain a number and special character";
 });
