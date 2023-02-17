6aba35ce113430a2e25104ccff4378e5a84ae217
David Lidebrandt <david.lidebrandt@gmail.com>
2023-02-17 20:50:01 +0100

    Change form username field

--- js/login.js
+++ js/login.js
@@ -1,13 +1,13 @@
 const loginForm = document.querySelector(".loginForm");
-const firstName = document.querySelector("#full_name");
+const userName = document.querySelector("#user_name");
 const password = document.querySelector("#password");
 const email = document.querySelector("#email");
 
 function submitLogin() {
   alert(
-    `Welcome ${firstName.value} with email ${email.value} and password ${password.value}, thank you for logging in.`
+    `Welcome ${userName.value} with email ${email.value} and password ${password.value}, thank you for logging in.`
   );
-  firstName.value = "";
+  userName.value = "";
   password.value = "";
   email.value = "";
 }
--- js/signUp.js
+++ js/signUp.js
@@ -1,13 +1,13 @@
 const signUpButton = document.querySelector(".signUpButton");
-const firstName = document.querySelector("#full_name");
+const userName = document.querySelector("#user_name");
 const password = document.querySelector("#password");
 const email = document.querySelector("#email");
 
 function submitSignUp() {
   alert(
-    `Welcome ${firstName.value} with email ${email.value} and password ${password.value}, thank you for signing up.`
+    `Welcome ${userName.value} with email ${email.value} and password ${password.value}, thank you for signing up.`
   );
-  firstName.value = "";
+  userName.value = "";
   password.value = "";
   email.value = "";
 }
